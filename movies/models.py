f2e88097c358eee5e4e9b97110ebce8dd94bb768
runedemonic <runedemonic@naver.com>
2022-09-30 10:18:59 +0900

    views.py

--- movies/models.py
+++ movies/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=80)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+
+    return render()
+
+def detail(request):
+
+    return render()
+
+def write(request):
+
+    return render()
+
+def modify(request):
+
+    return render()
+
+def delete(request):
+
+    return  render()
