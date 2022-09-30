d191912420004b7df5f3f4b065f9bf638b748d6b
runedemonic <runedemonic@naver.com>
2022-09-30 14:15:29 +0900

    complete

--- movies/urls.py
+++ movies/urls.py
@@ -6,7 +6,9 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('write/', views.write, name='write'),
-    # path('detail/', views.detail),
+    path('detail/<int:_id>', views.detail, name='detail'),
+    path('delete/<int:_id>', views.delete, name='delete'),
+    path('update/<int:_id>', views.update, name='update'),
     # path('new/', views.new),
     # path('edit/', views.edit),
 ] 
--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Post
+import datetime
+
 
 # Create your views here.
 def index(request):
-
     data = Post.objects.all().order_by('id')
 
     return render(request, 'movies/index.html', {'data': data})
 
-def detail(request):
-    pass
+
+def detail(request, _id):
+    data = Post.objects.get(id=_id)
+
+    return render(request, 'movies/detail.html', {'data': data})
+
 
 def write(request):
-
     _title = request.GET.get('title')
     _content = request.GET.get('content')
 
@@ -20,8 +24,18 @@ def write(request):
 
     return redirect('movies:index')
 
-def modify(request):
-    pass
 
-def delete(request):
-    pass
+def update(request, _id):
+    target = Post.objects.get(id=_id)
+    target.title = request.GET.get('title')
+    target.content = request.GET.get('content')
+    target.updated_at = datetime.date.today()
+    target.save()
+
+    return redirect('movies:detail', _id)
+
+
+def delete(request, _id):
+    Post.objects.get(id=_id).delete()
+
+    return redirect('movies:index')
